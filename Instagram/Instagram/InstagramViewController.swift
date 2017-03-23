cb4b5801836ecb57bc28d9bc45de5695b70c536d
Merrick Sapsford <merricks@apadmi.com>
2017-03-23 12:03:57 +0000

    Add instagram style

---
+++ Instagram/Instagram/InstagramViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InstagramViewController.swift
+//  Instagram
+//
+//  Created by Merrick Sapsford on 23/03/2017.
+//  Copyright © 2017 Merrick Sapsford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Podfile
+++ Podfile
@@ -4,10 +4,24 @@ workspace 'Tabman-Styles'
 
 use_frameworks!
 
+def shared_pods
+
+  pod 'Tabman', '0.4.1'
+
+end
+
 target 'Twitter' do
   project './Twitter/Twitter.xcodeproj'
   workspace 'Tabman-Styles'
 
-  pod 'Tabman', '0.4.1'
+  shared_pods
+
+end
+
+target 'Instagram' do
+  project './Instagram/Instagram.xcodeproj'
+  workspace 'Tabman-Styles'
+
+  shared_pods
 
 end
